bc494b0b0bf39f69682ff3f847c18b8c05ff4bdb
Zack Gilbert <zackgilbert@gmail.com>
2012-06-14 23:35:48 -0400

    Initial rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuckeredIn::Application.config.secret_token = '79ddbf59fe854f632598f80f19ddfc0138f0ffa1330250637203dbb08ca9c6c4df3753b65fb5cd3676646f5782cc592e40c6acf2cd922e5e691decd7fb7dfd08'
