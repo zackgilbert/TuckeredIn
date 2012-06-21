c212c007e17a2ade6d47c9db27a944fc612a7770
Zack Gilbert <zackgilbert@gmail.com>
2012-06-20 22:58:07 -0400

    Trying different fog_host value.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,15 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
-      :provider               => 'AWS',       # required
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
-      :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
+      :provider               => 'AWS',                               # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],        # required
+      :region                 => 'us-east-1'                          # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
-    config.fog_host       = 'https://s3.amazonaws.com/tuckeredin/'#'http://www.tuckered.in'            # optional, defaults to nil
-    config.fog_public     = true                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.fog_host       = 'https://s3.amazonaws.com/tuckeredin'     #'http://www.tuckered.in'            # optional, defaults to nil
+    config.fog_public     = true                                      # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}    # optional, defaults to {}
 
     # non-fog way of storing with s3?
     config.root = Rails.root.join('tmp')
