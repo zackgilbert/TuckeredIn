4218a6f4588b5802e2fd9ff68d8610ebcab8ddfc
Zack Gilbert <zackgilbert@gmail.com>
2012-12-09 00:46:10 -0600

    Adding ability to delete pending photos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TuckeredIn::Application.routes.draw do
-  
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   match "/dev_login" => "sessions#dev_login"
@@ -8,23 +8,23 @@ TuckeredIn::Application.routes.draw do
   match "/logout" => "sessions#destroy", :as => :signout
 
   get "/upload" => "photos#new", :as => 'upload'
-  
+
   get "/submit" => "photos#new", :as => 'new_photo'
   post "/submit" => "photos#create", :as => 'photos'
   get "/cuties/:id" => "photos#show", :as => "photo"
-  delete "/photos/:id" => "photos#destroy"#, :as => "photos"
+  delete "/photos/:id" => "photos#destroy", :as => "delete_photo"
   get "/photos/:id/edit" => "photos#edit", :as => "edit_photo"
   put "/photos/:id" => "photos#update", :as => "update_photo"
   post "/photos/:id/approve" => "photos#approve", :as => "approve_photo"
   delete "/photos/:id/unapprove" => "photos#unapprove", :as => "unapprove_photo"
   post "/photos/:id/like" => "photos#like", :as => "like_photo"
   delete "/photos/:id/unlike" => "photos#unlike", :as => "unlike_photo"
-  
+
   get "/pending" => "photos#pending", :as => 'pending'
 
-  get "/splash" => 'pages#splash'  
+  get "/splash" => 'pages#splash'
   get "/" => 'photos#index', :as => 'home'
   get "/cuties" => redirect("/")
   root :to => 'photos#index'
-  
+
 end
