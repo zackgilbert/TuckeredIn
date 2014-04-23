42fab9af85ec7eeab4c4efe95345eefd853d27db
Zack Gilbert <zackgilbert@gmail.com>
2014-04-23 12:04:49 -0500

    Delete stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,7 +0,0 @@
-# defaults to test key/secret
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||= "pk_test_U8PJlXcckz6yt0bDRyJlvRZd",
-  :secret_key      => ENV['STRIPE_SECRET_KEY'] ||= "sk_test_TMbSrvlENhzRXmduSlUpsMyD"
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
